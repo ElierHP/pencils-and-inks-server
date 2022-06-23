8207a0970480cc7aa7edf60e52b4eb0fcb29dd9b
ElierHP <elierporto@gmail.com>
2022-06-23 18:15:52 -0400

    Added product validation and tests.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+    # validations
+    validates :title, presence: true, length: {maximum: 100, minimum: 10}
+    validates :price, presence: true
+    validates :description, presence: true, length: {maximum: 1000, minimum: 20}
+    validates :sku, presence: true, length: {maximum: 20, minimum: 6}
+    validates :images, presence: true, length: {maximum: 500, minimum: 6}
+    validates :category, presence: true, length: {maximum: 20, minimum: 3}
+    validates :tags, length: {maximum: 50}
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,63 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @product = products(:one)
+  end
+  test "title should be valid" do
+    @product.title = ""
+    assert_not @product.valid?
+    @product.title = "a" * 101
+    assert_not @product.valid?
+    @product.title = "a" * 9
+    assert_not @product.valid?
+  end
+
+  test "price should exist" do
+    @product.price = nil
+    assert_not @product.valid?
+  end
+
+  test "description should be valid" do
+    @product.description = ""
+    assert_not @product.valid?
+    @product.description = "a" * 1001
+    assert_not @product.valid?
+    @product.description = "a" * 19
+    assert_not @product.valid?
+  end
+
+  test "sku should be valid" do
+    @product.sku = ""
+    assert_not @product.valid?
+    @product.sku = "a" * 21
+    assert_not @product.valid?
+    @product.sku = "a" * 5
+    assert_not @product.valid?
+  end
+
+  test "images should be valid" do
+    @product.images = ""
+    assert_not @product.valid?
+    @product.images = "a" * 5001
+    assert_not @product.valid?
+    @product.images = "a" * 5
+    assert_not @product.valid?
+  end
+
+  test "category should be valid" do
+    @product.category = ""
+    assert_not @product.valid?
+    @product.category = "a" * 21
+    assert_not @product.valid?
+    @product.category = "a" * 2
+    assert_not @product.valid?
+  end
+
+  test "tags should be valid" do
+    @product.tags = ""
+    assert @product.valid?
+    @product.tags = "a" * 51
+    assert_not @product.valid?
+  end
 end

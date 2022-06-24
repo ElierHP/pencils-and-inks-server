057f82ee58b0bf3cf1c9c65082be5d9f46c5bbdd
ElierHP <elierporto@gmail.com>
2022-06-24 17:27:30 -0400

    Add user controller actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,34 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
 
   def show
+    render json: {id: @user[:id], email: @user[:email]}
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: 'success', status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @user.destroy
   end
 
   private
     def set_user
-      @user = User.find(params[:email])
+      @user = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "products#index"
   
-  resources :users, only: [:show, :create, :update, :delete]
+  resources :users, only: [:show, :create, :update, :destroy]
 
   resources :products
 end

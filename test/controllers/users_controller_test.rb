c912e1d3f8fd3e27208f33e62d17acd9a45732b5
ElierHP <elierporto@gmail.com>
2022-06-24 17:38:53 -0400

    User controller tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,23 +1,34 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
   test "should get show" do
-    get users_show_url
+    get user_url(@user)
     assert_response :success
   end
 
-  test "should get create" do
-    get users_create_url
-    assert_response :success
+  test "should post to create" do
+    assert_difference("User.count") do
+      post users_url, params: {user: {email: 'example_user', password: 'secret', password_confirmation: 'secret', role: 'member'} }, as: :json
+    end
+
+    assert_response :created
   end
 
   test "should get update" do
-    get users_update_url
+    patch user_url(@user), params: {user: {email: 'example_user_2', password: 'secret2', password_confirmation: 'secret2', role: 'admin'} }, as: :json
+
     assert_response :success
   end
 
   test "should get destroy" do
-    get users_destroy_url
-    assert_response :success
+    assert_difference("User.count", -1) do
+      delete user_url(@user), as: :json
+    end
+
+    assert_response :no_content
   end
 end

81346ffd68490c6ffcf3c0da49f4dbd7331de577
ElierHP <elierporto@gmail.com>
2022-06-27 17:46:27 -0400

    Test session routes.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get login" do
-    get sessions_login_url
+  setup do
+  @user = User.create(email: 'example@user.com', password: 'secret', password_confirmation: 'secret', role: 'member')
+  end
+
+  test "should send post req to /login & authenticate user" do
+    post login_url, params: {session: { email: 'example@user.com', password: 'secret'}}, as: :json
+
     assert_response :success
   end
 
-  test "should get logout" do
-    get sessions_logout_url
-    assert_response :success
+  test "should send delete request to /logout" do
+    delete logout_url
+    assert_response :no_content
   end
 end

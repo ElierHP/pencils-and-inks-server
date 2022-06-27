8114ca126052de85a4a508c4b3a88c75abe7451c
ElierHP <elierporto@gmail.com>
2022-06-27 17:15:20 -0400

    Create sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  
+  # POST /login
+  def create
+  user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      render json: 'success', status: :ok
+    else
+      render json: 'unauthorized', status: :unauthorized
+    end
+  end
+  
+  # DELETE /logout
+  def destroy
+    log_out
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :create, :update, :destroy]
 
   resources :products
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end

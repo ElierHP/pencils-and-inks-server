838850fdc01519d92080f500070f6884e7ad013b
ElierHP <elierporto@gmail.com>
2022-08-02 19:48:59 -0400

    Fix: error with single cart item.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,13 +1,33 @@
 class CartController < ApplicationController
     # GET /cart
     def index
-        render json: session[:cart]
+        @products = []
+
+        # Check if there's a cart session
+        if session[:cart]
+            # Check if there's multiple items
+            if session[:cart].include?(',')
+                @cart_ids = session[:cart].split(',');
+                @cart_ids = @cart_ids.map do |id|
+                    id.to_i
+                end
+                @products = Product.find(@cart_ids)
+            else
+                # Run this if there's only 1 item.
+                @product = Product.find(session[:cart])
+                # Turn single product into array.
+                @products = [@product]
+            end
+        end
+
+        # Returns empty array if there is nothing in cart.
+        render json: @products
     end
 
     # POST /cart
     def create
         @product = Product.find_by(id: params[:product_id])
-        @similar_item = session[:cart].include?(params[:product_id].to_s)
+        @similar_item = session[:cart].include?(params[:product_id].to_s) unless session[:cart] == nil
 
         # Checks that product exists and is not already in the cart.
         if @product && !@similar_item
@@ -25,10 +45,16 @@ class CartController < ApplicationController
 
     # DELETE /cart/1
     def destroy
-        @cart_session = session[:cart].split(',');
-        @cart = @cart_session.filter do |id|
-            id != params[:id].to_s
+        # If there's more than 1 item in cart, run this.
+        if session[:cart].include?(',')
+            @cart_session = session[:cart].split(',');
+            @cart = @cart_session.filter do |id|
+                id != params[:id].to_s
+            end
+            session[:cart] = @cart.join(',')
+        # Else delete session
+        else
+            session[:cart] = nil
         end
-        session[:cart] = @cart.join(',')
     end
 end
